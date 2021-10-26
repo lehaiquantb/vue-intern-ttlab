cd7c655cb4d81b83cbb27044d49e17fc95d69d51
lehaiquantb <lehaiquantb@gmail.com>
2021-10-26 18:00:58 +0700

    add tuan2

---
+++ src/modules/tuan2/components/Card1.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="card1">
+        <div class="card1_icon">
+            <img :src="require('@/assets/images/bai1/' + card.icon)" />
+        </div>
+        <div class="card1_name">
+            {{ card.name }}
+        </div>
+        <p class="card1_des">
+            {{ card.description }}
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    components: {},
+    props: {
+        card: {
+            type: Object,
+        },
+    },
+})
+export default class Card1 extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.card1 {
+    background-color: #fff;
+    border-radius: 16px;
+    height: 197.87px;
+    padding: 20px;
+    &_icon {
+        margin-bottom: 10px;
+        height: 24px;
+    }
+    &_name {
+        margin-bottom: 10px;
+        font-size: 24px;
+    }
+    &_des {
+        font-size: 16px;
+    }
+}
+</style>
---
+++ src/modules/tuan2/route.ts
@@ -0,0 +1,34 @@
+import { RouteRecordRaw } from 'vue-router';
+import Bai1 from './pages/Bai1.vue';
+import Bai2 from './pages/Bai2.vue';
+import AssignmentLayout from '@/layouts/AssignmentLayout.vue';
+
+export default [
+    {
+        path: '/tuan1',
+        component: AssignmentLayout,
+        meta: {
+            public: true,
+        },
+        children: [
+            {
+                path: 'bai1_1',
+                name: 'bai1_1',
+                component: Bai1,
+                meta: {
+                    onlyWhenLoggedOut: true,
+                    public: true,
+                },
+            },
+            {
+                path: 'bai1_2',
+                name: 'bai1_2',
+                component: Bai2,
+                meta: {
+                    onlyWhenLoggedOut: true,
+                    public: true,
+                },
+            },
+        ],
+    },
+] as RouteRecordRaw[];
