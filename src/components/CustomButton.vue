0d8849bd2d8f0c4beb6312234197fb139f6c2bc4
lehaiquantb <lehaiquantb@gmail.com>
2021-11-02 18:20:11 +0700

    up

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="btn-custom"></div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    props: {
+        title: { type: String, default: '' },
+        disabled: { type: Boolean, default: false },
+    },
+})
+export default class CustomButton extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.btn-custom {
+}
+</style>
